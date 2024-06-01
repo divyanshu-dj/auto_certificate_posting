815804aaea8c04c9dbc78a18afff32b686b2df9c
divyanshu-dj <jhadivyanshu042@gmail.com>
2024-06-01 13:37:44 +0530

    1

--- util/postToTwitter.js
+++ util/postToTwitter.js
@@ -3,11 +3,11 @@ import { TwitterApi } from "twitter-api-v2";
 export default async function postToTwitter( outCertificatePath, prAuthor, prTitle) {
     try {
         const client = new TwitterApi({
-            appKey: process.env.TWITTER_CONSUMER_KEY || "SfMetP0OJr24ivXwD5ycthTNO",
-            appSecret: process.env.TWITTER_CONSUMER_SECRET || "11jtrHjL3Tgj1031IXkyCCQOZTvpBem81eYdOEaNge3VvcLTcl",
-            bearerToken: process.env.TWITTER_BEARER_TOKEN || "AAAAAAAAAAAAAAAAAAAAAKJFuAEAAAAAtOvW55PmiOKfyDjtv1FVfRaSAuo%3DpaY8V2YJ2ErrjAiUWrEtKV2eFQohkMWr68m2RBOWZSWEzQHXPK",
-            accessToken: process.env.TWITTER_ACCESS_TOKEN || "1796530445368115200-36kMB6FTAxm9e2dEOuxnb7Ycg6tXDj",
-            accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET || "R60kD3XbgGJPtVmRd4voBSEXkLKrssUTVOeu5khsOPMtF",
+            appKey: "",
+            appSecret: "",
+            bearerToken: "",
+            accessToken: "",
+            accessSecret: "",
         });
 
         const mediaId = await client.v1.uploadMedia(outCertificatePath, { type: 'png' });
