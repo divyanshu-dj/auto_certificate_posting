582cd2a20ec5a4ce31fdf2f9fb770822c58b456b
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2024-06-01 19:50:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ import postToTwitter from "./util/postToTwitter.js";
 const prAuthor = process.env.GITHUB_ACTOR;
 const prTitle = process.env.PR_TITLE;
 
-const outCertificatePath = `${prAuthor}-${prTitle.toLowerCase()}.png`;
+const outCertificatePath = `${prAuthor}-${prTitle.replace(/\W+/g, '_').toLowerCase()}.png`;
 
-generateCertificate(prAuthor, prTitle, outCertificatePath).then(() =>
-    console.log("Certificate generated.")
-);
+generateCertificate(prAuthor, prTitle, outCertificatePath)
 
-postToTwitter(outCertificatePath, prAuthor, prTitle).then(() =>
-    console.log("Posted to Twitter.")
-);
+postToTwitter(outCertificatePath, prAuthor, prTitle)
