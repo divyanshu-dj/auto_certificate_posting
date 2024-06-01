50a52a9f8bebb7e26f6e68043589f15c730c450d
divyanshu-dj <jhadivyanshu042@gmail.com>
2024-06-01 13:56:48 +0530

    done

--- util/postToTwitter.js
+++ util/postToTwitter.js
@@ -3,11 +3,11 @@ import { TwitterApi } from "twitter-api-v2";
 export default async function postToTwitter( outCertificatePath, prAuthor, prTitle) {
     try {
         const client = new TwitterApi({
-            appKey: "",
-            appSecret: "",
-            bearerToken: "",
-            accessToken: "",
-            accessSecret: "",
+            appKey: process.env.TWITTER_CONSUMER_KEY,
+            appSecret: process.env.TWITTER_CONSUMER_SECRET,
+            bearerToken: process.env.TWITTER_BEARER_TOKEN,
+            accessToken: process.env.TWITTER_ACCESS_TOKEN,
+            accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
         });
 
         const mediaId = await client.v1.uploadMedia(outCertificatePath, { type: 'png' });
