000d33a99dfc7b595b5554725dd8c867f1f179a6
Divyanshu Kumar Jha <116970264+divyanshu-dj@users.noreply.github.com>
2024-06-02 12:37:11 +0530

    Update postToTwitter.js

--- util/postToTwitter.js
+++ util/postToTwitter.js
@@ -1,16 +1,17 @@
 import { TwitterApi } from "twitter-api-v2";
+import fs from "fs";
 
 export default async function postToTwitter( outCertificatePath, prAuthor, prTitle) {
+    const client = new TwitterApi({
+        appKey: process.env.TWITTER_CONSUMER_KEY,
+        appSecret: process.env.TWITTER_CONSUMER_SECRET,
+        bearerToken: process.env.TWITTER_BEARER_TOKEN,
+        accessToken: process.env.TWITTER_ACCESS_TOKEN,
+        accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
     try {
-        const client = new TwitterApi({
-            appKey: process.env.TWITTER_CONSUMER_KEY,
-            appSecret: process.env.TWITTER_CONSUMER_SECRET,
-            bearerToken: process.env.TWITTER_BEARER_TOKEN,
-            accessToken: process.env.TWITTER_ACCESS_TOKEN,
-            accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-        });
-
-        const mediaId = await client.v1.uploadMedia(outCertificatePath, { type: 'png' });
+        const image = fs.readFileSync(outCertificatePath);
+        const mediaId = await client.v1.uploadMedia(image, { type: 'png' });
         await client.v2.tweet({
             text: `Congratulations to ${prAuthor} for their PR merge: ${prTitle}! 🎉`,
             media: { media_ids: [mediaId] },
